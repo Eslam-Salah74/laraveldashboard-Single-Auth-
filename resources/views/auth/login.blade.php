666355b41910a3e99e105d1389ca6921c449ccb2
Eslam-Salah74 <eslamsalah20003000@gmail.com>
2024-11-27 20:08:51 +0200

    Login By Phone Number

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,9 +29,9 @@
                                                 <form method="POST" action="{{ route('login') }}">
                                                  @csrf
 													<div class="form-group">
-													<label>البريد الالكتروني</label>
-                                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                                     @error('email')
+														<label>البريد الإلكتروني أو رقم الهاتف</label>
+														<input id="login" type="text" class="form-control @error('login') is-invalid @enderror" name="login" value="{{ old('login') }}" required autocomplete="login" autofocus>
+                                                     @error('login')
                                                      <span class="invalid-feedback" role="alert">
                                                      <strong>{{ $message }}</strong>
                                                      </span>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 
 /*
@@ -22,6 +23,8 @@ Route::get('/', function () {
 // Auth::routes();
 Auth::routes(['register'=>false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    
+});
 Route::get('/{page}',[AdminController::class,'index']);
